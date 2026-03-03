89408886837ea2d7e296fb7a253c9683e747caad
Shihab <mohammadshihabuddin7@gmail.com>
2026-03-04 05:57:47 +0600

    activation mail

--- phibook/urls.py
+++ phibook/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
-from .views import api_root
+from .views import api_root, activate_account
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
@@ -38,6 +38,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',api_root),
+    path('activate/<uid>/<token>/', activate_account, name='activate-account'),
     # path('auth/', include('djoser.urls')),
     # path('auth/', include('djoser.urls.jwt')),
     # path('api-auth/', include('rest_framework.urls')),
--- phibook/views.py
+++ phibook/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import redirect
+from django.contrib.auth import get_user_model
+from django.utils.http import urlsafe_base64_decode
+from django.contrib.auth.tokens import default_token_generator
+from django.http import HttpResponse, HttpResponseBadRequest
+
 
 def api_root(request):
     return redirect('api-root')
+
+
+def activate_account(request, uid, token):
+    if request.method != "GET":
+        return HttpResponseBadRequest("Invalid request method.")
+
+    try:
+        user_id = urlsafe_base64_decode(uid).decode()
+        user = get_user_model().objects.get(pk=user_id)
+    except (TypeError, ValueError, OverflowError, get_user_model().DoesNotExist):
+        return HttpResponseBadRequest("Invalid activation link.")
+
+    if default_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save(update_fields=["is_active"])
+        return HttpResponse("Your account has been activated successfully.")
+
+    return HttpResponseBadRequest("Activation link is invalid or expired.")
 
